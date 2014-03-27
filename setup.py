ae6fb960153844230b624006b87fba0649605955
Andrew Droffner <adroffner@visual-a.com>
2014-03-27 12:38:29 -0400

    Add setup.py to allow distutils etc.

---
+++ setup.py
@@ -0,0 +1,38 @@
+#! /usr/bin/env python
+#
+# Tastypie Searchable ModelResource Objects
+# ======================================================================
+try:
+    # Allow Python eggs bdist_egg
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+from tastypie_search import __version__
+
+readme = open('README.md', 'r')
+README_TEXT = readme.read()
+readme.close()
+
+setup(name='django-tastypie-searchable',
+      version=__version__,
+      license='MIT Licensed',
+      description='Tastypie Searchable ModelResource using Haystack',
+      long_description=README_TEXT,
+      author='Andrew Droffner',
+      author_email='adroffner@gmail.com',
+      url='https://github.com/adroffner/tastypie-searchable',
+      download_url='',
+      packages=['tastypie_search', ],
+      ## scripts=[''],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          ],
+     )
+
---
+++ tastypie_search/__init__.py
@@ -0,0 +1,4 @@
+# Tastypie Searchable ModelResource Objects
+# ======================================================================
+
+__version__ = '0.01'
