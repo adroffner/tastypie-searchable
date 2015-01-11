f870b93547a97cd0b76601c450da4fc1ec2649bb
CentOS 6.5 <adroffner@localhost.localdomain>
2015-01-11 05:58:44 -0800

    Move the sample application into a sample_app directory, with unit tests. We should verify which Django version this is...

---
+++ tastypie_search/sample_app/__init__.py
@@ -0,0 +1,11 @@
+""" Sample Application: Tastypie Searchable ModelResource Objects
+
+This sample django applications has a Searchable Tastypie REST API.
+One ModelResource is indexed into Haystack and supports full-text search.
+
+Django Tastypie and Haystack must be installed to make this useful.
+This only connects the two using a simple Mixin.
+"""
+
+__version__ = '0.01'
+
